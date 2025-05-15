b05c61d8b829d10db26db7691d2acea466c34f85
Colin Gross <grosscol@gmail.com>
2025-05-15 10:42:32 -0400

    Revert use of command line variables. Use single front matter of first slide.

--- build_slides.sh
+++ build_slides.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env sh
 
-DATE="${DATE:=$(date '+%Y-%m-%d')}"
-
 # Run from project root directory to create index.html
 pandoc -t revealjs --template=pandoc-templates/default.revealjs \
   -o index.html  --slide-level=2 \
-  -V revealjs-url=reveal.js  -V theme=serif slides/*.md -V date="${DATE}"
+  -V revealjs-url=reveal.js  -V theme=serif slides/*.md
 
 echo "index.html built $(date)"
 
